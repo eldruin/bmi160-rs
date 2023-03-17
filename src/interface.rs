a179e10058fce7ad08e26411098e81c0a16ec062
Diego Barrios Romero <eldruin@gmail.com>
2023-03-17 07:46:28 +0100

    Fix clippy warnings

--- src/interface.rs
+++ src/interface.rs
@@ -71,7 +71,7 @@ where
 
     fn write_data(&mut self, payload: &mut [u8]) -> Result<(), Self::Error> {
         let addr = self.address;
-        self.i2c.write(addr, &payload).map_err(Error::Comm)
+        self.i2c.write(addr, payload).map_err(Error::Comm)
     }
 }
 
@@ -94,7 +94,7 @@ where
     fn write_data(&mut self, payload: &mut [u8]) -> Result<(), Self::Error> {
         self.cs.set_low().map_err(Error::Pin)?;
         payload[0] += 0x80;
-        let result = self.spi.write(&payload).map_err(Error::Comm);
+        let result = self.spi.write(payload).map_err(Error::Comm);
 
         self.cs.set_high().map_err(Error::Pin)?;
         result
@@ -148,9 +148,9 @@ where
         Ok(result?[1])
     }
 
-    fn read_data(&mut self, mut payload: &mut [u8]) -> Result<(), Self::Error> {
+    fn read_data(&mut self, payload: &mut [u8]) -> Result<(), Self::Error> {
         self.cs.set_low().map_err(Error::Pin)?;
-        let result = self.spi.transfer(&mut payload).map_err(Error::Comm);
+        let result = self.spi.transfer(payload).map_err(Error::Comm);
         self.cs.set_high().map_err(Error::Pin)?;
         result?;
         Ok(())
