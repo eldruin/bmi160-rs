a9b31d2f77be9e3b6f7073aac41823d6a1065365
deets <deets@web.de>
2023-10-07 12:00:41 +0200

    Implement integration tests for scaled data retrieval

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,4 +1,7 @@
-use bmi160::{Data, MagnetometerData, Sensor3DData, SensorSelector};
+use bmi160::scaling::{DataScaled, Sensor3DDataScaled};
+use bmi160::{
+    AccelerometerRange, Data, GyroscopeRange, MagnetometerData, Sensor3DData, SensorSelector,
+};
 mod common;
 use crate::common::{destroy_i2c, destroy_spi, new_i2c, new_spi, Register, DEV_ADDR};
 use embedded_hal_mock::{i2c::Transaction as I2cTrans, pin::Mock as PinMock};
@@ -120,6 +123,56 @@ mod get_sensor_data {
             }),
             accel: None,
             time: None,
+        };
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn acc_and_gyr_scaled_default() {
+        let mut imu = new_i2c(&[I2cTrans::write_read(
+            DEV_ADDR,
+            vec![Register::MAG],
+            BUFFER.to_vec(),
+        )]);
+        let result = imu.scaled_data(SensorSelector::all()).unwrap();
+        let expected = DataScaled {
+            gyro: Some(Sensor3DDataScaled {
+                x: 156.64635,
+                y: 187.98781,
+                z: 219.32927,
+            }),
+            accel: Some(Sensor3DDataScaled {
+                x: 0.25091553,
+                y: 0.2822876,
+                z: 0.31365967,
+            }),
+            time: Some(0x171615),
+        };
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn acc_and_gyr_scaled_changed() {
+        let mut imu = new_i2c(&[
+            I2cTrans::write(DEV_ADDR, vec![Register::GYR_RANGE, 0b010]),
+            I2cTrans::write(DEV_ADDR, vec![Register::ACC_RANGE, 0b0101]),
+            I2cTrans::write_read(DEV_ADDR, vec![Register::MAG], BUFFER.to_vec()),
+        ]);
+        imu.set_gyro_range(GyroscopeRange::Range500s);
+        imu.set_accel_range(AccelerometerRange::Range4g);
+        let result = imu.scaled_data(SensorSelector::all()).unwrap();
+        let expected = DataScaled {
+            gyro: Some(Sensor3DDataScaled {
+                x: 39.161587,
+                y: 46.996952,
+                z: 54.832317,
+            }),
+            accel: Some(Sensor3DDataScaled {
+                x: 0.50183105,
+                y: 0.5645752,
+                z: 0.62731934,
+            }),
+            time: Some(0x171615),
         };
         assert_eq!(result, expected);
     }
